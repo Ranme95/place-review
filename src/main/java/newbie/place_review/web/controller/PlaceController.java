3ffc16f102c7cec421793cf3559e8c8d2502618c
ranme <qkralfks3@naver.com>
2025-02-14 17:46:38 +0900

    Fix: PlaceController 검색결과 페이지 추가
    Feat: 장소 검색 결과 html 생성

--- src/main/java/newbie/place_review/web/controller/PlaceController.java
+++ src/main/java/newbie/place_review/web/controller/PlaceController.java
@@ -3,12 +3,17 @@ package newbie.place_review.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/place")
 @Controller
 public class PlaceController {
 
-    @GetMapping("/place/{placeId}")
+    @GetMapping("/{placeId}")
     String initPlaceDetails(@PathVariable("placeId") Long placeId) {
         return "pages/place/place-details";
     }
+
+    @GetMapping("/search")
+    String initPlaceSearch(){return "pages/place/place-search";}
 }
